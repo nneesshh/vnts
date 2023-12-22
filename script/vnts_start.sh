c67d0399038c70a9b67b3a7e115e3691e139e934
nneesshh <nneesshh@163.com>
2023-12-22 11:45:09 +0800

    add script

---
+++ script/vnts_start.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+startwork_release()
+{
+	cd ../bin/
+	echo "begin start vnts"
+
+  	#export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
+	#export LD_PRELOAD="libjemalloc.so.2"
+
+	nohup ./vnts --port 30001 --white-token abcdefg41 --gateway 10.10.0.1 --netmask 255.255.255.0 & echo $! > vnts.pid
+	
+	echo "end start vnts"
+	cd ../script/
+}
+
+stopwork_release()
+{
+    cd ../bin/
+    echo "begin stop vnts"
+    #pkill server;
+    #kill -9 $(sed -n 1p server.pid);
+    kill -9 `cat vnts.pid`
+    sleep 2
+    echo "end stop vnts"
+    cd ../script/
+
+}
+
+startwork_debug()
+{
+	cd ../bin/Debug/
+	echo "begin start vnts"
+
+  	#export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
+	#export LD_PRELOAD="libjemalloc.so.2"
+
+	nohup ./vnts --port 30001 --white-token abcdefg41 --gateway 10.10.0.1 --netmask 255.255.255.0 & echo $! > vnts.pid
+	
+	echo "end start vnts"
+	cd ../../script/
+}
+
+stopwork_debug()
+{
+    cd ../bin/Debug/
+    echo "begin stop vnts"
+    #pkill server;
+    #kill -9 $(sed -n 1p server.pid);
+    kill -9 `cat vnts.pid`
+    sleep 2
+    echo "end stop vnts"
+    cd ../../script/
+
+}
+
+
+    case $1 in
+        -d)
+            stopwork_debug
+	        startwork_debug
+        ;;
+        -r)
+            stopwork_release
+	        startwork_release
+        ;;
+        *)
+            stopwork_release
+            startwork_release
+        ;;
+   esac
